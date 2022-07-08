3232538d80838eb923037f21523ea26e95e7a16e
Mckennastan <oikenna3@gmail.com>
2022-07-08 12:46:26 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,12 +7,12 @@
   */
 void print_numbers(void)
 {
-	int x;
+int x;
 
-	for (x = 0; x <= 9; x++)
-	{
-		_putchar(x + '0');
-	}
+for (x = 0; x <= 9; x++)
+{
+_putchar(x + '0');
+}
 
-	_putchar('\n');
+_putchar('\n');
 }
