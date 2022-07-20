ced02c69d22106b3cb7e0f1286edde0d2d85e69b
Mckennastan <oikenna3@gmail.com>
2022-07-20 04:49:56 -0700

    A program that prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string folowed by a new line
- * @s: string
- * Return: Always (0)
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string to print
+ *
+ * Return: void
  */
-
 void _puts_recursion(char *s)
 {
-if (*s == '\0')
-_putchar('\n');
-else
-{
-_putchar(*s);
-_puts_recursion(++s);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
-}
-Foot
