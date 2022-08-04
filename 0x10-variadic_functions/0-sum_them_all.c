7c598c1bce5dc69685747395c66811ed3c415a7d
Mckennastan <oikenna3@gmail.com>
2022-08-04 10:41:27 -0700

    variadic functions for several data type

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,25 +1,35 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 
 /**
- * sum_them_all - Returns the sum of all its paramters.
- * @n: The number of paramters passed to the function.
- * @...: A variable number of paramters to calculate the sum of.
- *
- * Return: If n == 0 - 0.
- *         Otherwise - the sum of all parameters.
+ * sum_them_all - sum of all its parameters.
+ *@n: num
+ * Return: Sum.
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list nums;
-	unsigned int index, sum = 0;
+	va_list sum_num;
+	double sum = 0;
+	unsigned int i;
 
-	va_start(nums, n);
+	/* initialize valist for num number of arguments */
+	va_start(sum_num, n);
 
-	for (index = 0; index < n; index++)
-		sum += va_arg(nums, int);
+	if (n == 0)
+	{
+		return (0);
+	}
 
-	va_end(nums);
+	/* access all the arguments assigned to valist */
+	i = 0;
+	while (i < n)
+	{
+		sum += va_arg(sum_num, int);
+		i++;
+	}
+
+	/* clean memory reserved for valist */
+	va_end(sum_num);
 
 	return (sum);
 }
